eb5637706a97badabd7c1b051d53986bd62756b1
François Philippon <franswa56@hotmail.fr>
2024-02-06 01:09:26 +0100

    finalisation

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -22,7 +22,7 @@ const EventList = () => {
   data?.events
   .filter(event => !type || event.type === type)
   
-  .filter((index) => {
+  .filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
